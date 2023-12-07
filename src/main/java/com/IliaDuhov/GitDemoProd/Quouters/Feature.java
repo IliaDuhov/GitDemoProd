8e27e12aec68426e75e0eac6c3da00ae85ae79e6
Ilia_Duhov <dukhov03@mail.ru>
2023-12-08 00:47:22 +0400

    Feature 1 commit

---
+++ src/main/java/com/IliaDuhov/GitDemoProd/Quouters/Feature.java
@@ -0,0 +1,11 @@
+package com.IliaDuhov.GitDemoProd.Quouters;
+
+public class Feature {
+    private String name;
+    private long id;
+
+    public Feature(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+}
