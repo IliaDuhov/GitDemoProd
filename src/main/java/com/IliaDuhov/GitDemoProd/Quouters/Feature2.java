0f73921ece166e16272a612e7a7c172eb6653e9f
Ilia_Duhov <dukhov03@mail.ru>
2023-12-08 01:06:10 +0400

    ThirdFeature commit 1

---
+++ src/main/java/com/IliaDuhov/GitDemoProd/Quouters/Feature2.java
@@ -0,0 +1,23 @@
+package com.IliaDuhov.GitDemoProd.Quouters;
+
+public class Feature2 {
+    private String name;
+    private long id;
+
+    public Feature2(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
