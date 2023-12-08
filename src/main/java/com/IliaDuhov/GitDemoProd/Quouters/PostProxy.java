5c9bfcda7324ccd6e9d2216d86295411ab40b885
Ilia <dukhov03@mail.ru>
2023-12-08 11:31:56 +0400

    Update PostProxy.java

--- src/main/java/com/IliaDuhov/GitDemoProd/Quouters/PostProxy.java
+++ src/main/java/com/IliaDuhov/GitDemoProd/Quouters/PostProxy.java
@@ -2,7 +2,7 @@ package com.IliaDuhov.GitDemoProd.Quouters;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
+//comment
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PostProxy {
 }
